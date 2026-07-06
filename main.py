c36ebaea752e2fcefd719f15bf3012f8a04ee434
felipe camboim <felipe.clicklog@gmail.com>
2026-07-06 16:21:20 -0300

    Estrtura final e limpeza na raiz

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+# Adiciona a pasta ProcessoChecklist ao caminho do sistema
+sys.path.append(os.path.abspath("ProcessoChecklist"))
+
+# Importa o código que está dentro da pasta
+import Checklist_veicular
